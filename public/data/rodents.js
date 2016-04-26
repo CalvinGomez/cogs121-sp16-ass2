a664bf914fc2dcb2259f898ea1dc999ae126ec82
Calvin Gomez <calvin.gomez1@gmail.com>
2016-04-26 01:21:26 -0700

    Added circle

---
+++ public/data/rodents.js
@@ -0,0 +1,12 @@
+var rodents_json = {
+
+"type": "FeatureCollection",                                                                                
+"features": [{ 
+	"type": "Feature", 
+	"geometry": {
+		"type": "Point", 
+		"coordinates": [-117.391480, 33.5]
+	}  
+
+}]
+}
